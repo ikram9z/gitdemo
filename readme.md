1b89365a949456b1fc00f94071ccc3447d4ddfa6
Ikram <ikramwani51@gmail.com>
2025-06-01 16:02:54 +0530

    Add new data

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 Hello World ;)
+
+Author - John Doe
---
+++ script.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const script = () => {
+  return (
+    <div>
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+export default script
